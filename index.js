d20f86cd1b43f8bd114ca5d15974a75fcb7ddc9b
ZhoupengWu <182263202+ZhoupengWu@users.noreply.github.com>
2026-05-19 22:43:43 +0200

    Tasto tutorial nella home e cambio denominazione

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const button_start = /** @type {HTMLElement} */ (document.getElementById("start-here"));
+const button_tutorial = /** @type {HTMLElement} */ (document.getElementById("tutorial-btn"));
 
 button_start.addEventListener("click", () => {
     location.href = "./assets/pages/list_game.html";
+});
+
+button_tutorial.addEventListener("click", () => {
+    location.href = "./assets/pages/tutorial.html";
 });
